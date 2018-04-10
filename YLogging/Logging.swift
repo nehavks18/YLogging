653d2533cb276692367b56ffd19ea3a9ee719fb2
Neha_headerlabs <neha@headerlabs.com>
2018-04-10 10:25:33 +0530

    Initial Commit

---
+++ YLogging/Logging.swift
@@ -0,0 +1,39 @@
+//
+//  Logging.swift
+//  YLogging
+//
+//  Created by Neha on 4/10/18.
+//  Copyright © 2018 Neha. All rights reserved.
+//
+
+import Foundation
+
+public class YLog
+{
+    //1
+    private var isDebug : Bool!
+    
+    //2
+    public init()
+    {
+        self.isDebug = false
+    }
+    
+    //3
+    public func setup(isDebug : Bool)
+    {
+        self.isDebug = isDebug
+        print("Project in debug mode : \(isDebug)")
+        
+    }
+    
+    //4.
+    public func YPrint<T>(value: T) {
+        if self.isDebug == true {
+            print(value)
+        } else {
+            //Do any stuff for production mode
+        }
+
+}
+}
